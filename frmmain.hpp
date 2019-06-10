19fc3321e38c97b01272177670718903c34c3767
Gerald Büchler <dev@genomast.de>
2019-06-10 16:02:17 +0200

    added empty QT-Widget Project

---
+++ frmmain.hpp
@@ -0,0 +1,22 @@
+#ifndef FRMMAIN_HPP
+#define FRMMAIN_HPP
+
+#include <QMainWindow>
+
+namespace Ui {
+class FrmMain;
+}
+
+class FrmMain : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit FrmMain(QWidget *parent = nullptr);
+    ~FrmMain();
+
+private:
+    Ui::FrmMain *ui;
+};
+
+#endif // FRMMAIN_HPP
