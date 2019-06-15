441f125b824f9fd2983cbc5cd866d194af025661
Gerald Büchler <dev@genomast.de>
2019-06-15 21:03:05 +0200

    fill the status-bar message

--- frmmain.cpp
+++ frmmain.cpp
@@ -8,9 +8,23 @@ FrmMain::FrmMain(QWidget *parent) :
     ui->setupUi(this);
     /* Connects for the Menue */
     connect(ui->actionExit,&QAction::triggered,this,&FrmMain::close);
+
+    this->updateStatus();
 }
 
 FrmMain::~FrmMain()
 {
     delete ui;
+}
+
+void FrmMain::updateStatus()
+{
+    if (ui->lstPassword->count() == 1)
+    {
+        ui->statusBar->showMessage(QString::number(ui->lstPassword->count()) + " " + tr(" Entry"));
+    }
+    else
+    {
+        ui->statusBar->showMessage(QString::number(ui->lstPassword->count()) + " " + tr(" Entries"));
+    }
 }
--- frmmain.hpp
+++ frmmain.hpp
@@ -17,6 +17,7 @@ public:
 
 private:
     Ui::FrmMain *ui;
+    void updateStatus();
 };
 
 #endif // FRMMAIN_HPP
