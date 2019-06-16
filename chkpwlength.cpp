6fb5fd24f21c9646beb2792f827efc510343c30f
Gerald Büchler <dev@genomast.de>
2019-06-16 21:42:06 +0200

    added an empty class "chkpwlength"

---
+++ chkpwlength.cpp
@@ -0,0 +1,6 @@
+#include "chkpwlength.hpp"
+
+chkPWLength::chkPWLength()
+{
+
+}
---
+++ chkpwlength.hpp
@@ -0,0 +1,12 @@
+#ifndef CHKPWLENGTH_HPP
+#define CHKPWLENGTH_HPP
+
+#include "icheck.hpp"
+
+class chkPWLength : public iCheck
+{
+public:
+    chkPWLength();
+};
+
+#endif // CHKPWLENGTH_HPP
