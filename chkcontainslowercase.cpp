cd76019a87628df1d905e55da37b6704ba7bf20d
Gerald Büchler <dev@genomast.de>
2019-06-29 22:06:22 +0200

    implement the class (chkcontainslowercase)

--- chkcontainslowercase.cpp
+++ chkcontainslowercase.cpp
@@ -2,5 +2,22 @@
 
 ChkContainsLowerCase::ChkContainsLowerCase()
 {
+    this->re = new QRegExp("[a-z]{1}");
+}
 
+ChkContainsLowerCase::~ChkContainsLowerCase()
+{
+    delete this->re;
+}
+
+bool ChkContainsLowerCase::check(QString s)
+{
+    for(int i=0; i <= s.length(); ++i)
+    {
+        if (this->re->exactMatch(QString(s[i])))
+        {
+            return true;
+        }
+    }
+    return false;
 }
--- chkcontainslowercase.hpp
+++ chkcontainslowercase.hpp
@@ -1,12 +1,17 @@
 #ifndef CHKCONTAINSLOWERCASE_HPP
 #define CHKCONTAINSLOWERCASE_HPP
 
+#include <QRegExp>
 #include "icheck.hpp"
 
 class ChkContainsLowerCase : public iCheck
 {
 public:
     ChkContainsLowerCase();
+    ~ChkContainsLowerCase() override;
+    bool check(QString s) override;
+private:
+    QRegExp *re;
 };
 
 #endif // CHKCONTAINSLOWERCASE_HPP
