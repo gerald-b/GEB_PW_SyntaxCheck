981d6dffc4156f2afe590e5ba6c54540f7d4b26f
Gerald Büchler <dev@genomast.de>
2019-06-29 22:36:49 +0200

    implement the class (chkcontainsnumber)

--- chkcontainsnumber.cpp
+++ chkcontainsnumber.cpp
@@ -2,5 +2,23 @@
 
 ChkContainsNumber::ChkContainsNumber()
 {
+    this->re = new QRegExp("[0-9]{1}");
+}
 
+ChkContainsNumber::~ChkContainsNumber()
+{
+    delete re;
+}
+
+bool ChkContainsNumber::check(QString s)
+{
+
+    for(int i=0; i <= s.length(); ++i)
+    {
+        if (this->re->exactMatch(QString(s[i])))
+        {
+            return true;
+        }
+    }
+    return false;
 }
--- chkcontainsnumber.hpp
+++ chkcontainsnumber.hpp
@@ -1,12 +1,17 @@
 #ifndef CHKCONTAINSNUMBER_HPP
 #define CHKCONTAINSNUMBER_HPP
 
+#include <QRegExp>
 #include "icheck.hpp"
 
 class ChkContainsNumber : public iCheck
 {
 public:
     ChkContainsNumber();
+    ~ChkContainsNumber() override;
+    bool check(QString s) override;
+private:
+    QRegExp *re;
 };
 
 #endif // CHKCONTAINSNUMBER_HPP
