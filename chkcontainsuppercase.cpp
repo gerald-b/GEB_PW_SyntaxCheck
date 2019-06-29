bd1d4c89b19f799d9f54ca4efe40ece40ceb3420
Gerald Büchler <dev@genomast.de>
2019-06-29 21:13:15 +0200

    added content to class

--- chkcontainsuppercase.cpp
+++ chkcontainsuppercase.cpp
@@ -4,3 +4,16 @@ ChkContainsUpperCase::ChkContainsUpperCase()
 {
 
 }
+
+bool ChkContainsUpperCase::check(QString s)
+{
+    QRegExp re("[A-Z]{1}");
+    for(int i=0; i <= s.length(); ++i)
+    {
+        if (re.exactMatch(QString(s[i])))
+        {
+            return true;
+        }
+    }
+    return false;
+}
--- chkcontainsuppercase.hpp
+++ chkcontainsuppercase.hpp
@@ -1,12 +1,14 @@
 #ifndef CHKCONTAINSUPPERCASE_HPP
 #define CHKCONTAINSUPPERCASE_HPP
 
+#include <QRegExp>
 #include "icheck.hpp"
 
 class ChkContainsUpperCase : public iCheck
 {
 public:
     ChkContainsUpperCase();
+    bool check(QString s) override;
 };
 
 #endif // CHKCONTAINSUPPERCASE_HPP
